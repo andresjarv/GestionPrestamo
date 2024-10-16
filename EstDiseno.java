62aaa9fcf72bd85fc2c474067ec6ed450149fede
jose manuel alvarez <jalvarezalvarez30@gmail.com>
2024-10-16 07:29:10 -0500

    constructor superclase

--- EstDiseno.java
+++ EstDiseno.java
@@ -5,7 +5,8 @@ public class EstDiseno extends Estudiante{
     private int cantAsignatura;//Asignaturas que esta viendo este semestre
     
 
-    public EstDiseno(String modEstudio, int cantAsignatura) {
+    public EstDiseno(String cedula,String nombre,String apellido,String telefono,String serial,String modEstudio, int cantAsignatura) {
+        super(cedula, nombre, apellido, telefono, serial);
         this.modEstudio = modEstudio;
         this.cantAsignatura = cantAsignatura;
     }
